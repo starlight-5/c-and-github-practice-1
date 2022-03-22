1e04faef933c11a033962ddb9eae8d76a93387a8
starlight-5 <apep10012@naver.com>
2022-03-22 18:19:57 +0900

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
- 
 int main(void)
-{printf("hello");}
+{
+   int num = 12;
+   printf("%d\n", num);
 
-
+   printf("%d %d \n", num, num++);
+   printf("%d %d\n", num++, num);
+   printf("%d %d %d %d %d", ++num, num++, num++, num, ++num);
+   return 0;
+}
