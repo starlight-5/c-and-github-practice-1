530eb6849edd635740140a1a8a037272b6af6972
starlight-5 <apep10012@naver.com>
2022-03-22 21:04:59 +0900

    second commit

--- main.c
+++ main.c
@@ -3,7 +3,6 @@ int main(void)
 {
    int num = 12;
    printf("%d\n", num);
-   printf("hello");
    printf("%d %d \n", num, num++);
    printf("%d %d %d %d %d", ++num, num++, num++, num, ++num);
    return 0;
