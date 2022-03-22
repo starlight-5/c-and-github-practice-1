3d34e9e7b2822554d7739207c1ccc654c5f588fe
starlight-5 <apep10012@naver.com>
2022-03-22 22:03:40 +0900

    five commit

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-   int num = 12;
-   printf("%d\n", num);
-   printf("hello");
-   printf("%d %d \n", num, num++);
-   printf("%d %d %d %d %d", ++num, num++, num++, num, ++num);
-   return 0;
+   int a=10,b=0,c=0;
+   int num1,num2,num3,num4,num5;
+   num1= a && b;
+   num2= a || b;
+   num3= (a>b) && (a<c);
+   num4= !c;
+   num5= b && c;
+   printf("%d \n %d \n %d \n %d \n %d",num1,num2,num3, num4,num5);
 }
