267ffe8b801c889d9d3bdff63e5b04974691c0fb
starlight-5 <apep10012@naver.com>
2022-04-13 00:53:32 +0900

    first commit

---
+++ g.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int num=1,num1;
+    printf("정수를 입력하세요:");
+    scanf("%d",&num1);
+    while(num<=num1){
+        printf("%d \n",num);
+        num++;
+    }
+    return 0;
+}
