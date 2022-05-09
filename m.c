e4b32694c5844fdf2992bcdb83519a4733a98142
starlight-5 <apep10012@naver.com>
2022-05-09 15:08:30 +0900

    first commit

--- m.c
+++ m.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 int main(void)
 {
-    int num, num1;
-    printf("정수 두개 입력: ");
-    scanf("%d %d", &num, &num1);
-    while (num == 0 || num1 == 0)
+    int num = 1;
+    int sum = 0;
+    for (num = 1; num <= 100; num++)
     {
-
-        printf("정수 두개 다시 입력: ");
-        scanf("%d %d", &num, &num1);
+        if (num%10==6)
+        {
+            continue;
+        }
+        
+        sum += num;
+        if (num % 5 == 0)
+        {
+            sum += num;
+        }
+        if (sum >= 2000)
+        {
+            break;
+        }
     }
-    if (num > num1)
-        printf("%d", num - num1);
-    else
-        printf("%d", num1 - num);
+    printf("%d", sum);
     return 0;
 }
