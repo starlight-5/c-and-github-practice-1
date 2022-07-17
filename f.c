04334694076b74df89792efb41ce0990cb959018
starlight-5 <apep10012@naver.com>
2022-07-17 17:32:04 +0900

    first commit

---
+++ f.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int num, num1, dan;
+    for (num = 1; num <= 9; num++)
+    {
+        for (dan=1; dan <= 9; dan++)
+        {   if(dan%2==0)
+            printf("%d x %d = %2d ", dan, num, dan * num);
+        }
+    printf("\n");
+    }
+    
+    return 0;
+}
